ac9b440a1a1eac6a2e5124d8e121aa73fc2e0add
Thai Vo <tahrbo@gmail.com>
2025-04-28 14:24:34 -0500

    Exercise 1 Skeleton

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Testing .gitignore");
+            Random r = new Random();
+            int favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Guess my favorite number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < r)
+            {
+                Console.WriteLine($"Too low!\nYou guessed: {userInput}");
+            }
+            else if (userInput > r)
+            {
+                Console.WriteLine($"Too high!\nYou guessed: {userInput}");
+            }
+            else
+                Console.WriteLine("Exactly!");
+            
         }
     }
 }
